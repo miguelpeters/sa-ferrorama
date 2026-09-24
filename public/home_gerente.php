8b81787e20355f509c33110bc53056329a68a130
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-09-24 09:29:06 -0300

    add - desenvolvi a página home do gerente

---
+++ public/home_gerente.php
@@ -0,0 +1,53 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <title>Home Gerente</title>
+
+    <link rel="icon" href="../assets/imgs/PageIcon.png" type="image/x-icon">
+</head>
+
+<body class="body_home">
+
+    <header class="cabecalho">
+    
+    <div class="lado-esquerdo">
+        <a class="home-detrain-logo" href="../index.php">DE-TRAIN</a>
+        <img id="icon" src="../assets/imgs/LogoDeTrain.png" class="logo">
+
+
+        <nav class="menu">
+        <a class="home-detrain-logo" href="../index.php">CADASTRAR FUNCIONÁRIO</a>
+        <a class="home-detrain-logo" href="cadastro_trem.php">CADASTRAR TREM</a>
+        <a class="home-detrain-logo" href="cadastro_estacao.php">CADASTRAR ESTAÇÃO</a>
+        <a class="home-detrain-logo" href="cadastro_sensor.php">CADASTRAR SENSOR</a>
+        </nav>
+    </div>
+
+    <div class="lado-direito">
+        <h1><a class="EntrarText_home" href="login.php">ENTRAR</a></h1>
+        <img src="../assets/imgs/LoginIcon.png" class="perfil">
+    </div>
+
+</header>
+
+<main class="main_home">
+    <img src="../assets/imgs/sistema-metrô-joinvilense.webp" alt="Linhas" class="linhas_home">
+</main>
+
+
+<footer class="footer_home">
+
+        <h2 class="slogan_footer_home">Melhor ir DE-TRAIN!</h2>
+
+
+</footer>
+
+</body>
+
+
+
+
+</html>
